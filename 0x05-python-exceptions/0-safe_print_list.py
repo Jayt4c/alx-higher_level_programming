73e1a612c4ecde38de7fee46c6efe673a59270f6
Jayt4c <igejuwon53@gmail.com>
2024-01-22 06:10:39 -0800

    task 1 sorted

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     printed = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print(my_list[i], end='')
             printed += 1
-    except IndexError:
-        #print("The list is too short, index is not available.")
-        pass
+        except IndexError:
+            break#print("The list is too short, index is not available.")
     print()
     return (printed)
