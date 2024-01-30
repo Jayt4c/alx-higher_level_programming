ef3bdda12ec807b49abb1bee2608b1f4ed63af4b
Jayt4c <igejuwon53@gmail.com>
2024-01-30 04:42:21 -0800

    101 task redo 2

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""Defines a locked class."""
+
+
 class LockedClass:
-    __slots__ = ['first_name']
-    def __init__(self):
-        pass
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
+
+    __slots__ = ["first_name"]
